f1633a429eba6a428bea3656dd54a725f23660bb
nanu2 <mike.helderman@gmail.com>
2014-09-16 12:36:33 +0200

    added try catch

--- ldappr/api.py
+++ ldappr/api.py
@@ -1,8 +1,13 @@
+import sys
+import ldap
 from connection import Connection, AuthConnection
 
 
 def connect_to(server, *args, **kwargs):
-    if args or 'bind_dn' and 'password' in kwargs:
-    #if args:
-        return AuthConnection(server, *args, **kwargs)
-    return Connection(server, **kwargs)
+    try:
+        if args or 'bind_dn' and 'password' in kwargs:
+            return AuthConnection(server, *args, **kwargs)
+        return Connection(server, **kwargs)
+    except Exception as e:
+        print str(e)
+
